eae663e11053088b021cab081aff84067ffc5048
Patrick Chin <60618726+patchinco@users.noreply.github.com>
2020-10-18 13:19:29 +0200

    Break a test

--- src/Tests/IdentityRoleTests.cs
+++ src/Tests/IdentityRoleTests.cs
@@ -35,7 +35,9 @@
 
 			var role = new IdentityRole(name);
 
-			Expect(role.Name, Is.EqualTo(name));
+			//Break test on purpose
+			//Expect(role.Name, Is.EqualTo(name));
+			Expect(role.Name, Is.EqualTo("notadmin"));
 		}
 
 		[Test]
